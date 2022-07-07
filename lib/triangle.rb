384d876c4f4bff0519e04673240546f32a3144d8
+ <+>
2022-07-07 08:44:56 -0400

    commit tree 92b1f6936e01415c37c3bd17de2634c722e50619

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+
+  attr_accessor :sideA, :sideB, :sideC
+
+  def initialize (sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind
+    if (@sideA <= 0 || @sideB <= 0 || @sideC <= 0)
+      raise TriangleError
+    elsif ((@sideA + @sideB <= sideC) || (@sideA + @sideC <= @sideB || (@sideB + @sideC <= @sideA)))
+      raise TriangleError
+    else
+      if (self.sideA == self.sideB && self.sideA == self.sideC && self.sideB == self.sideC )
+        :equilateral
+      elsif ((self.sideA == self.sideB && self.sideA != self.sideC) ||
+        (self.sideA == self.sideC && self.sideA != self.sideB) ||
+        (self.sideB == self.sideC && self.sideA != self.sideC))
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def sizeError
+      "side has no size or invalide size"
+    end
+
+    def triangleInequality
+      "side totals can not make triangle"
+    end
+  end
+
 end
